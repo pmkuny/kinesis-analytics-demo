4f0189916f95f3be8d1ecfa93ae71e5b282c187b
Patrick Kuny <pmk62810@gmail.com>
2021-04-15 07:57:01 -0500

    Co-authored-by: Alec Dutcher <aidutcher@users.noreply.github.com>

--- newuser.py
+++ newuser.py
@@ -5,12 +5,14 @@ import requests
 import uuid
 import botocore.exceptions
 
+# Defining global variables for our various services clients.
 g_api_endpoint = "https://randomuser.me/api"
 g_s3_client = boto3.client("s3")
 g_firehose_client = boto3.client("firehose", region_name='us-west-2')
 g_firehose_role_arn = 'arn:aws:iam::695507447459:role/KinesisFirehosetoS3'
 
-
+# The create_delivery_stream method of Firehose requires one of several destinations.
+# We're using S3 here so we'll use this function to create an S3 bucket.
 def create_firehose_bucket(bucket_name):
     bucket_name = bucket_name + '-' + '{}'.format(uuid.uuid4())
     boto_response = g_s3_client.create_bucket(Bucket=bucket_name)
@@ -24,6 +26,8 @@ def create_firehose_delivery_stream(delivery_stream_name,destination_bucket):
     if delivery_stream_name == None:
         delivery_stream_name = 'firehoseds-' + '{}'.format(uuid.uuid4())
         
+    # Role ARN is specifying the universal identifier (ARN) of the IAM Role that Firehose will use to write to the S3 bucket.
+    # Bucket ARN is meant to be dynamic and based on bucket name.
     try:
         g_firehose_client.create_delivery_stream(
             DeliveryStreamName=delivery_stream_name,
@@ -38,7 +42,7 @@ def create_firehose_delivery_stream(delivery_stream_name,destination_bucket):
         
     
     
-    
+# Run in parallel to get the user API data. Store as JSON.
 def get_user_api_data(api_endpoint):
     endpoint_response = requests.get(api_endpoint)
     endpoint_response_json = endpoint_response.json()
